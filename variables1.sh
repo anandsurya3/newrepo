c958e1f9b45b5386a5414d8fd3ae97fee3d46f7b
Anand <kallaanand0@gmail.com>
2025-12-30 14:51:04 +0530

    somemessage

---
+++ variables1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "hi to every one "
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi guys, how are you both $PERSON2 and $PERSON3 doing?"
+echo "$PERSON2: "We are fine! How about you $PERSON1 and $PERSON3?"
+echo "$PERSON1: I am good too $PERSON2 . I wanted to ask—would you both rather go to the waterpark or the theater this"
+echo "$PERSON2: I think the waterpark sounds like more fun $PERSON3."
+echo "$PERSON3: Yeah, I agree $PERSON1 ! We’ve been to the theater so many times lately $PERSON2 . Let's go to the waterpark."
+echo "$PERSON1: Great choice $PERSON3 . We should probably head out early in the morning to beat the $PERSON2 crowds."
+echo "$PERSON2: Sounds like a plan $PERSON1 . See you then $PERSON3!"
+echo "$PERSON1: Bye! See you Sunday $PERSON3 and $PERSON2."
+
+echo "this is final"
