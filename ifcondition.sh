dbc35debe4659875ebdd188eb692d01d0258a152
Anand <kallaanand0@gmail.com>
2026-01-04 07:16:06 +0530

    somemessage

--- ifcondition.sh
+++ ifcondition.sh
@@ -4,11 +4,10 @@ echo "please enter the pin :"
 
 read NUMBER
 
-if [ $NUMBER -le 10 ]; then
+if [ $NUMBER -lt 10 ]; then
  echo "given number is less then 10"
  elif [ $NUMBER -eq 10 ]; then
   echo "given number is equal to 10"
   else 
    echo "given number is grater then 10"
    fi
-   
