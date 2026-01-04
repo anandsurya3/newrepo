ad7e5740d9b5aed31042a517a15a176c56c561e2
Anand <kallaanand0@gmail.com>
2026-01-04 13:17:16 +0530

    somemessage

--- variables1.sh
+++ variables1.sh
@@ -3,13 +3,16 @@
 echo "please enter the number : "
 
 read NUMBER 
-if [ $(( $NUMBER % 2 )) -eq 0]; then 
+ if [ $(($NUUMBER % 2 )) -eq 0 ]; then 
+  echo "given $NUMBER is even"
+  else 
+   echo "given $NUMBER is odd"
+   fi
 
- echo "given $NUMBER is even"
- else
-  echo "given $NUMBER is odd "
-  fi 
+   $PWD 
 
-  $PWD
-
+for i in {1..35}
+do
+ echo $i
+ done
  
