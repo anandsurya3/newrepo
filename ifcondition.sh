ef486f46389eaa29e140f3b5765040f4878fcb98
Anand <kallaanand0@gmail.com>
2026-01-04 06:32:28 +0530

    somemessage

--- ifcondition.sh
+++ ifcondition.sh
@@ -5,8 +5,9 @@ echo "pls enter the pin :"
 read NUMBER
 
 if [ $NUMBER -le 15 ]; then
- echo "given number is working"
+ echo "given number less then 15"
+ elif 
+ echo "given number equal to 15"
   else
-  echo "given number is not working"
+  echo "given number grater then 15"
   fi
-  
