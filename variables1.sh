8c70522816d6aa4affc80b226d3ec8a07eca1038
Anand <kallaanand0@gmail.com>
2026-01-04 08:52:09 +0530

    somemessage

--- variables1.sh
+++ variables1.sh
@@ -1,43 +1,12 @@
-#!/bin/bash
+#!/bin.bash
 
-echo "hi to every one "
+echo "please enter the number"
 
-PERSON1=$1
-PERSON2=$2
-PERSON3=$3
+read NUMBER
 
-
-echo "$PERSON1 : Hey $PERSON2, hey $PERSON3! I’m so glad we finally managed to get all three of us together today."""""""""
-echo "$PERSON2 : I agree, $PERSON1! It feels like forever since $PERSON3 and I have seen you."
-echo "$PERSON3 : It really has been a while, $PERSON2. I was just telling $PERSON1 the other day that we need to make these hangouts a regular thing."
-echo "$PERSON1 : That’s a great idea, $PERSON3. So, $PERSON2, do you have any suggestions for what we should do this weekend?"
-echo "$PERSON2 : Well, $PERSON1, I was thinking about that new exhibit downtown, but I want to know if $PERSON3 is interested in art."
-echo "$PERSON3 : To be honest, $PERSON2, I’m more in the mood for something outdoorsy, but I’ll go wherever you and $PERSON1 want to go."
-echo "$PERSON1 : Since $PERSON3 is being flexible, $PERSON2, maybe we can find a park that has an outdoor art installation?"
-echo "$PERSON2 : That is a perfect compromise, $PERSON1! What do you think of that plan, $PERSON3?"
-echo "$PERSON3 : I love it, $PERSON2. Good thinking, $PERSON1! Let’s lock it in."
-
-
-
-
-
-anand=500
-surya=1000
-
-sum=$(($anand+$surya))
-
-echo "${sum}"
-
-echo "$PWD"
-
-x=30
-
-echo "x"
-
-echo "$x"
-
-echo y=$((25+10))
-
-
-echo $((x+y))
-
+if [ $(($NUMBER % 2)) -eq 10 ]
+ echo "given number is even "
+ else
+  echo "given number is odd "
+  fi
+  
