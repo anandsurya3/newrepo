cae9cfc9ffc8a84ff31cf155e4a6535c25d6d190
Anand <kallaanand0@gmail.com>
2026-01-04 08:53:03 +0530

    somemessage

--- variables1.sh
+++ variables1.sh
@@ -4,9 +4,8 @@ echo "please enter the number"
 
 read NUMBER
 
-if [ $(($NUMBER % 2)) -eq 10 ]
+if [ $(($NUMBER % 2)) -eq 10 ]; then
  echo "given number is even "
  else
   echo "given number is odd "
   fi
-  
