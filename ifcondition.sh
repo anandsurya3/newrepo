1d23f75a0f94e06c858bbdb019ffa5d230e228e1
Anand <kallaanand0@gmail.com>
2026-01-04 06:41:03 +0530

    somemessage

--- ifcondition.sh
+++ ifcondition.sh
@@ -6,6 +6,8 @@ read NUMBER
 
 if [ $NUMBER -le 15 ]; then
  echo "given $NUMBER less then 15"
+ elif [ $NUMBER -eq 15 ];
+ echo "given $NUMBER equl to 15"
   else
   echo "given $NUMBER grater then 15"
   fi
