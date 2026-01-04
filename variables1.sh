74885b5ecd31d588162fdc2c527df59ecdfc2d5e
Anand <kallaanand0@gmail.com>
2026-01-04 13:05:43 +0530

    somemessage

--- variables1.sh
+++ variables1.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-echo "please enter the number :"
+echo "please enter the number : "
 
-read NUMBER
+read NUMBER 
+if [ $(( $NUMBER % 2 )) -eq 0]; then 
 
-if [ $(($NUMBER % 2)) -eq 0 ]; then
  echo "given $NUMBER is even"
- else 
-  echo "given $NUMBER odd"
-  fi
+ else
+  echo "given $NUMBER is odd "
+  fi 
+
+  $PWD
+
+  userid=$(id -u)
+   echo $userid
+
+   x+y=$((1500*5))
+echo $x+y
+
+echo for i in {1..48}
+do
+ echo $i
+ done
+
+ 
