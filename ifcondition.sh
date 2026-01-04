aed938eb166731c5dcf3dd7c451c9a76b22b7842
Anand <kallaanand0@gmail.com>
2026-01-04 06:28:29 +0530

    somemessage

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-PERSON=$1
+echo "pls enter the pin :"
 
-if [ $PERSON -ge 15 ]; then
- echo "$PERSON is alive"
-else
- echo "$PERSON is no more"
- 
-fi
+read NUMBER
+
+if [ $NUMBER -le 15 ]; then
+ echo "given number is working"
+  else
+  echo "given number is not working"
+  fi
+  
