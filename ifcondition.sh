9ad374bd43c9cccc88488c1826dac1abc3d6bb86
Anand <kallaanand0@gmail.com>
2026-01-04 13:45:23 +0530

    somemessage

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-echo "please enter the number : "
 
-read NUMBER 
-
-if [ $NUMBER -gt 15 ]; then 
-    echo "given $NUMBER is grater then 15"
-elif [ $NUMBER -eq 15 ]; then 
- echo " given $NUMBER is equl to 15"
-
-else
-  echo " given $NUMBER is less then 15"
-fi 
 
   NUMBER=$1
 
 if [ $NUMBER - lt 25 ]; then
   echo " given $NUMBER is less then 25"
 elif [ $NUMBER -eq 25 ]; then 
-echo " given $NUMBER is equal to 25"
+  echo " given $NUMBER is equal to 25"
 else 
  echo " given $NUMBER grater then 25"
 fi
+
